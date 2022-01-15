10a45a05223e7fed1196e38a1324778b08da9186
yurippo <83097615+yurippo@users.noreply.github.com>
2022-01-15 08:06:09 -0800

    Added files via upload

---
+++ Hello GitHub/src/com/yurippo/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.yurippo.git;
+
+/**
+ * @author marci
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
